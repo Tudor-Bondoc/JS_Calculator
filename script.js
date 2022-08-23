7f3735cf623b3294645497836a803869ee13e110
Tudor Bondoc <tudor.bondoc2012@gmail.com>
2022-08-23 14:29:36 +0300

    Responsive + some bugs

--- script.js
+++ script.js
@@ -7,56 +7,60 @@ var result = 0;
 var remeberOperator;
 var numberOfOperators = 0;
 var numberOfConsOp = 0;
+var pressedEqual = false;
 
-function resetConsOp() {
+function isInteger(n) {
+    return n % 1 === 0;
+}
+
+function resetConsOp() { //reset the number of consecutive operators pressed
     numberOfConsOp = 0;
 }
 
-function switchMember2() {
-    member1 = parseInt(member1);
+function resetEqual(pressedEqual) {
+    pressedEqual = !pressedEqual;
+}
+
+function switchMember2() { //switch to the second member (input)
+    member1 = parseFloat(member1);
     stillMember1 = false;
 }
 
-function add1() {
+function add1() { //pressing the button 1
     resetConsOp();
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '1';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '1';
         document.querySelector("#display").innerHTML = member2;
-    }   
+    }
 }
 
 function add2() {
+
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '2';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '2';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add3() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '3';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '3';
         document.querySelector("#display").innerHTML = member2;
     }
@@ -65,130 +69,113 @@ function add3() {
 function add4() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '4';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '4';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add5() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '5';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '5';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add6() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '6';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '6';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add7() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '7';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '7';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add8() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '8';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '8';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add9() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '9';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '9';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 function add0() {
     resetConsOp();
 
-    if (stillMember1 == true)
-    {
+    if (stillMember1 == true) {
         member1 = member1 + '0';
         document.querySelector("#display").innerHTML = member1;
     }
-    else 
-    {
+    else {
         member2 = member2 + '0';
         document.querySelector("#display").innerHTML = member2;
     }
-   
+
 }
 
 
 
 function pressPlus() {
     numberOfConsOp++;
-    if(numberOfConsOp >= 2)
-    {
+    if (numberOfConsOp >= 2) {
         return;
     }
     numberOfOperators++;
-    if(numberOfOperators==1)
-    {
+    if (numberOfOperators == 1) {
         switchMember2();
     }
-    else
-    {
+    else {
         pressEqualwoutno();
     }
     remeberOperator = '+';
@@ -196,18 +183,15 @@ function pressPlus() {
 
 function pressMinus() {
     numberOfConsOp++;
-    if(numberOfConsOp >= 2)
-    {
+    if (numberOfConsOp >= 2) {
         return;
     }
     numberOfOperators++;
-    
-    if(numberOfOperators==1)
-    {
+
+    if (numberOfOperators == 1) {
         switchMember2();
     }
-    else
-    {
+    else {
         pressEqualwoutno();
     }
     remeberOperator = '-';
@@ -215,18 +199,15 @@ function pressMinus() {
 
 function pressMul() {
     numberOfConsOp++;
-    if(numberOfConsOp >= 2)
-    {
+    if (numberOfConsOp >= 2) {
         return;
     }
     numberOfOperators++;
-    
-    if(numberOfOperators==1)
-    {
+
+    if (numberOfOperators == 1) {
         switchMember2();
     }
-    else
-    {
+    else {
         pressEqualwoutno();
     }
     remeberOperator = '*';
@@ -234,18 +215,15 @@ function pressMul() {
 
 function pressDiv() {
     numberOfConsOp++;
-    if(numberOfConsOp >= 2)
-    {
+    if (numberOfConsOp >= 2) {
         return;
     }
     numberOfOperators++;
-    
-    if(numberOfOperators==1)
-    {
+
+    if (numberOfOperators == 1) {
         switchMember2();
     }
-    else
-    {
+    else {
         pressEqualwoutno();
     }
     remeberOperator = '/';
@@ -255,58 +233,55 @@ function pressEqual() {
     if (numberOfOperators == 0)
         return;
     numberOfOperators = 0;
-    member2 = parseInt(member2);
-    if (remeberOperator == '+')
-    {
+    pressedEqual = true;
+    member2 = parseFloat(member2);
+    if (remeberOperator == '+') {
         result = member1 + member2;
     }
-    else if(remeberOperator == '-')
-    {
+    else if (remeberOperator == '-') {
         result = member1 - member2;
     }
-    else if(remeberOperator == '*')
-    {
+    else if (remeberOperator == '*') {
         result = member1 * member2;
     }
-    else
-    {
+    else {
         result = member1 / member2;
     }
-    document.querySelector("#display").innerHTML = result;
+    if (!isInteger(result))
+        document.querySelector("#display").innerHTML = result.toFixed(4);
+    else
+        document.querySelector("#display").innerHTML = result;
     member1 = result;
-    member2='';
+    member2 = '';
     result = 0;
 }
 
 function pressEqualwoutno() { // equal without resetting numberOfOperators
-    member2 = parseInt(member2);
-        if (remeberOperator == '+')
-        {
-            result = member1 + member2;
-        }
-        else if(remeberOperator == '-')
-        {
-            result = member1 - member2;
-        }
-        else if(remeberOperator == '*')
-        {
-            result = member1 * member2;
-        }
-        else
-        {
-            result = member1 / member2;
-        }
+    member2 = parseFloat(member2);
+    if (remeberOperator == '+') {
+        result = member1 + member2;
+    }
+    else if (remeberOperator == '-') {
+        result = member1 - member2;
+    }
+    else if (remeberOperator == '*') {
+        result = member1 * member2;
+    }
+    else {
+        result = member1 / member2;
+    }
+    if (!isInteger(result))
+        document.querySelector("#display").innerHTML = result.toFixed(4);
+    else
         document.querySelector("#display").innerHTML = result;
-        member1 = result;
-        member2='';
-        result = 0;
+    member1 = result;
+    member2 = '';
+    result = 0;
 }
 
 
 
-
-
-function fclear() {
+function fclear() { //reload the page
     location.reload(false);
 }
 
